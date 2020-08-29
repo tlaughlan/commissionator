db71824ce02be001d2e91da7cd705952b186e49f
tlaughlan <twlaughlan@gmail.com>
2020-08-29 14:58:04 +1000

    Include Spring initializr files

---
+++ src/main/java/com/tlaughlan/commissionator/CommissionatorApplication.java
@@ -0,0 +1,13 @@
+package com.tlaughlan.commissionator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommissionatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommissionatorApplication.class, args);
+	}
+
+}
