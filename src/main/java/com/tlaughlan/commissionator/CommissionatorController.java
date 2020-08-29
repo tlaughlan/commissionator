75b54f983fc3a037e70dd9d7c53f3301e5cc298d
tlaughlan <twlaughlan@gmail.com>
2020-08-29 17:31:20 +1000

    Fill out Controller

--- src/main/java/com/tlaughlan/commissionator/CommissionatorController.java
+++ src/main/java/com/tlaughlan/commissionator/CommissionatorController.java
@@ -1,4 +1,17 @@
 package com.tlaughlan.commissionator;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class CommissionatorController {
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "calculator";
+    }
+
 }
