038d216c492ac4c4f5c171f6319a9d4e4becb6e0
tlaughlan <twlaughlan@gmail.com>
2020-08-30 23:58:25 +1000

    Create CommissionRateCalculatorService

--- src/main/java/com/tlaughlan/commissionator/services/CommissionRateCalculatorService.java
+++ src/main/java/com/tlaughlan/commissionator/services/CommissionRateCalculatorService.java
@@ -1,4 +1,41 @@
 package com.tlaughlan.commissionator.services;
 
+import com.tlaughlan.commissionator.models.Commission;
+import com.tlaughlan.commissionator.pojos.CommissionRates;
+import com.tlaughlan.commissionator.pojos.RateRange;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommissionRateCalculatorService {
+
+    public static Float calculateCommissionRate(Commission commission) {
+        Float commissionRate = null;
+        RateRange rateRange = findRateRange(commission.getAchievement());
+        Float base = rateRange.getBase();
+        Float rate = rateRange.getRate();
+
+        if (base != null && rate != null) {
+            commissionRate = base + ((commission.getAchievement() - base) * rate);
+        }
+
+        return commissionRate;
+    }
+
+
+
+    private static RateRange findRateRange(Float achievement) {
+        RateRange rateRange = null;
+        CommissionRates commissionRates = new CommissionRates();
+
+        for (RateRange currentRateRange : commissionRates.getRateRanges()) {
+            if (currentRateRange.getAchievementFloor() <= achievement &&
+                    currentRateRange.getAchievementCeiling() > achievement) {
+                rateRange = currentRateRange;
+            }
+        }
+
+        return rateRange;
+    }
+
+
 }
