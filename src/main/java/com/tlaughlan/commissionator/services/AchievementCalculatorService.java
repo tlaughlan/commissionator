36587bc723e11fd18b826350465f3f5f1bd5df73
tlaughlan <twlaughlan@gmail.com>
2020-08-30 22:41:31 +1000

    Improve AchievementCalculatorService

--- src/main/java/com/tlaughlan/commissionator/services/AchievementCalculatorService.java
+++ src/main/java/com/tlaughlan/commissionator/services/AchievementCalculatorService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class AchievementCalculatorService {
 
+    private static Float achievementCap = 99.98f;
+
     public static Float calculateAchievement(Commission commission) {
         Float achievement = null;
         Integer actual = commission.getActual();
@@ -15,11 +17,19 @@ public class AchievementCalculatorService {
             achievement = roundToTwoDecimals((float) actual/target);
         }
 
-        return achievement;
+        return adjustForCap(achievement);
     }
 
     private static Float roundToTwoDecimals(Float number) {
         double roundedNumber = Math.round(number * 100.0) / 100.0;
         return (float) roundedNumber;
+    }
+
+    private static Float adjustForCap(Float achievement) {
+        if (achievement > achievementCap && achievement != null) {
+            return achievementCap;
+        } else {
+            return achievement;
+        }
     }
 }
