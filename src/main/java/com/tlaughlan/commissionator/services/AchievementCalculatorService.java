a8e24ebefc8c52334e184477d4f2f0779c26f05e
tlaughlan <twlaughlan@gmail.com>
2020-08-31 15:01:20 +1000

    AchievementCalculatorService improvements

--- src/main/java/com/tlaughlan/commissionator/services/AchievementCalculatorService.java
+++ src/main/java/com/tlaughlan/commissionator/services/AchievementCalculatorService.java
@@ -12,22 +12,29 @@ public class AchievementCalculatorService {
     private static final Logger LOGGER = LoggerFactory.getLogger(AchievementCalculatorService.class);
     private static final Float achievementCap = 99.98f;
 
+    /***
+     * This method calculates the achievement based off the actual and target of a commission. The method checks
+     * requisite values for null and fails early if this is the case. The error message in unspecific so as not to leak
+     * implementation detail.
+     *
+     * @param commission
+     * @return achievement
+     */
     public static Float calculateAchievement(Commission commission) {
-        Float achievement = null;
         Integer actual = commission.getActual();
         Integer target = commission.getTarget();
-
-        if (actual != null && target != null) {
-            achievement = CommissionatorUtils.roundToTwoDecimals((float) actual/target);
+        if (actual == null || target == null) {
+            throw new IllegalArgumentException("Error occurred during achievement calculation.");
         }
 
+        Float achievement = CommissionatorUtils.roundToTwoDecimals((float) actual/target);
         achievement = adjustForCap(achievement);
         LOGGER.info("Achievement calculated: " + achievement);
         return achievement;
     }
 
     private static Float adjustForCap(Float achievement) {
-        if (achievement > achievementCap && achievement != null) {
+        if (achievement > achievementCap) {
             return achievementCap;
         } else {
             return achievement;
