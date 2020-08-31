df015e63a2d14156573b05929ff08973eaa0ec45
tlaughlan <twlaughlan@gmail.com>
2020-08-31 15:48:51 +1000

    Util class improvements

--- src/main/java/com/tlaughlan/commissionator/util/CommissionatorUtils.java
+++ src/main/java/com/tlaughlan/commissionator/util/CommissionatorUtils.java
@@ -2,7 +2,17 @@ package com.tlaughlan.commissionator.util;
 
 public class CommissionatorUtils {
 
+    /***
+     * This simple method rounds the provided float up to the nearest two decimal places. If the provided number is null
+     * then an error is logged and null is returned instead.
+     *
+     * @param number
+     * @return roundedNumber
+     */
     public static Float roundToTwoDecimals(Float number) {
+        if (number == null) {
+            return null;
+        }
         double roundedNumber = Math.round(number * 100.0) / 100.0;
         return (float) roundedNumber;
     }
