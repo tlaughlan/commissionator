01f4bd8858ff2016f56a32159874f3f5eb761977
tlaughlan <twlaughlan@gmail.com>
2020-08-31 15:43:54 +1000

    CommissionService improvements

--- src/main/java/com/tlaughlan/commissionator/services/CommissionService.java
+++ src/main/java/com/tlaughlan/commissionator/services/CommissionService.java
@@ -12,20 +12,27 @@ public class CommissionService {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(CommissionService.class);
 
+    /***
+     * This method uses the other services in the program to produce the total commission figure. The method will fail
+     * early if requisite inputs are not initialised. Other requisite inputs such as "actual" and "target" are checked
+     * for within the appropriate service which handles them directly. The error message in unspecific so as not to leak
+     * implementation detail.
+     *
+     * @param commission
+     * @return
+     */
     public static Commission calculateTotalCommission(Commission commission) {
-        if (commission.getAchievement() == null) {
-            commission.setAchievement(AchievementCalculatorService.calculateAchievement(commission));
+        Integer motc = commission.getMotc();
+        if (motc == null) {
+            throw new IllegalArgumentException("Error occurred during total commission calculation.");
         }
 
-        if (commission.getCommissionRate() == null) {
-            commission.setCommissionRate(CommissionRateCalculatorService.calculateCommissionRate(commission));
-        }
+        commission.setAchievement(AchievementCalculatorService.calculateAchievement(commission));
+        commission.setCommissionRate(CommissionRateCalculatorService.calculateCommissionRate(commission));
 
-        if (commission.getTotalCommission() == null) {
-            Float totalCommission = commission.getMotc() * commission.getCommissionRate();
-            LOGGER.info("Total commission amount calculated: " + totalCommission);
-            commission.setTotalCommission(CommissionatorUtils.roundToTwoDecimals(totalCommission));
-        }
+        Float totalCommission = motc * commission.getCommissionRate();
+        LOGGER.info("Total commission amount calculated: " + totalCommission);
+        commission.setTotalCommission(CommissionatorUtils.roundToTwoDecimals(totalCommission));
 
         return commission;
     }
